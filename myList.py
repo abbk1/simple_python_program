0c778ee2204bc0e425f799ed794cfb0a0e7a13b3
abbk1 <107799754+abbk1@users.noreply.github.com>
2024-11-09 12:20:21 -0800

    Create myList.py
    list assignment

---
+++ myList.py
@@ -0,0 +1,20 @@
+
+myList = []
+myList.append(10)
+myList.append(20)
+myList.append(30)
+myList.append(40)
+
+myList.insert(1, 15)
+
+extd = [50, 60, 70]
+
+myList.extend(extd)
+
+myList.pop()
+
+myList.reverse()
+
+myList.sort()
+
+print(myList[3]) 
